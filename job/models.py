c3cb3fc81de853125b367d0a494bd115c5c14e05
lenovo1 <marwaalmothana@gmail.com>
2025-08-22 22:41:47 +0300

    added job models

--- job/models.py
+++ job/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
+
+Joptype=(
+  ('part time' , 'part time'),
+  ('Full time' , 'Full time'),
+)
 # Create your models here.
+class Job (models.Model):
+  title = models.CharField(max_length=100)
+  jobtype = models.CharField(max_length=15 ,choices=Joptype)
+  Description = models.TextField (max_length=1000)
+  vacancy = models.IntegerField(default=1)
+  published_at = models.DateTimeField(auto_now=True)
+  salary = models.IntegerField(default=0)
+
+  def __str__  (self):
+    return self.title
--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #myapps
+
+    'blog',
+    'job' ,
+    'contact',
+    'home',
+    'accounts',
+
 ]
 
 MIDDLEWARE = [
